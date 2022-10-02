aeacc1a7b3583b3d2f5a80eed8fa0b2e5af7e2d9
ksnd0297 <ksnd0297@gmail.com>
2022-10-03 01:16:47 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -7,11 +7,7 @@ function App() {
     <div className="App">
       {/* 왼쪽 카테고리 & 검색 부분 */}
       <div className="aside" />
-<<<<<<< HEAD
-      {/* 지도A  */}
-=======
-      {/* 지도  */}
->>>>>>> 0bc49518f44a448d82d9e8c55b51f523dbb4db2e
+      {/* 지도BB  */}
       <div className="main" />
     </div>
   );
