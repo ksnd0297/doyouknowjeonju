8785c51c09494a32c4219be3912eea607c8f6568
ksnd0297 <ksnd0297@gmail.com>
2022-10-03 01:12:56 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      {/* 왼쪽 카테고리 & 검색 부분 */}
+      <div className="aside" />
+<<<<<<< HEAD
+      {/* 지도A  */}
+=======
+      {/* 지도  */}
+>>>>>>> 0bc49518f44a448d82d9e8c55b51f523dbb4db2e
+      <div className="main" />
+    </div>
+  );
+}
+
+export default App;
