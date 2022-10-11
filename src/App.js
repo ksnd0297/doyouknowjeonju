046439f67ba5dad1cb5c6eae03a865bbc6a6f200
sangyong <tkddyd1126@gmail.com>
2022-10-11 16:28:18 +0900

    10/11

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import ModalApp from "./components/Modal/Modal_App";
 import "./App.scss";
 
 function App() {
@@ -7,8 +7,10 @@ function App() {
     <div className="App">
       {/* 왼쪽 카테고리 & 검색 부분 */}
       <div className="aside" />
-      {/* 지도BBCCC  */}
-      <div className="main" />
+      {/* 지도BBCCD  */}
+      <div className="main">
+        <ModalApp />
+      </div>
     </div>
   );
 }
---
+++ src/components/Modal/Modal_App.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import Modal from "./Modal";
+
+function ModalApp() {
+  // useState를 사용하여 open상태를 변경한다. (open일때 true로 만들어 열리는 방식)
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      <button onClick={openModal}>병원 마크</button>
+      <Modal open={modalOpen} close={closeModal} header="Modal heading">
+        운영시간 : <p />
+        <br />
+        진료과목 : <p />
+        <br />
+        주소 : <p />
+        <br />
+        휴무일 : <p />
+        <br />
+        전화번호 : <p />
+        <br />
+      </Modal>
+    </React.Fragment>
+  );
+}
+
+export default ModalApp;
