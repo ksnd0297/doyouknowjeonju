d3b656ac390faf97d3fac9ed3a98f28763fd20d9
sangyong <tkddyd1126@gmail.com>
2022-10-14 00:58:21 +0900

    modal add

--- src/components/Modal/ModalParking.js
+++ src/components/Modal/ModalParking.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../../scss/modal.css";
 
-export const Modal = (props) => {
+export const ModalParking = (props) => {
   // 열기, 닫기, 모달 헤더 텍스트를 부모로부터 받아옴
   const { open, close, header } = props;
 
@@ -13,11 +13,23 @@ export const Modal = (props) => {
         <section>
           <header>
             {header}
-            <button className="close" onClick={close}>
+            <button className="close" onClick={close} img="../../scss/star.ico">
+              {/*X표시*/}
               {/* &times; */}
             </button>
           </header>
-          <main>{props.children}</main>
+          <main>
+            주소 : <p />
+            <br />
+            운영시간 : <p />
+            <br />
+            주차 총 대수 : <p />
+            <br />
+            가격 : <p />
+            <br />
+            전화번호 : <p />
+            <br />
+          </main>
           <footer>
             <button className="close" onClick={close}>
               close
