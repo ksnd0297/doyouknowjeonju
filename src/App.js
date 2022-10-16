3c88dcdd5607da1a9fb0c77b50845a023da80011
sangyong <tkddyd1126@gmail.com>
2022-10-16 17:59:06 +0900

    즐겨찾기 구현

--- src/App.js
+++ src/App.js
@@ -13,6 +13,12 @@ function App() {
   const closeModal = () => {
     setModalOpen(0);
   };
+  const Modal_hospitalFavorite = () => {
+    console.log("병원 즐겨찾기 추가됨");
+  };
+  const Modal_parkingFavorite = () => {
+    console.log("주차장 즐겨찾기 추가됨");
+  };
   return (
     <div className="App">
       {/* 왼쪽 카테고리 & 검색 부분 */}
@@ -24,6 +30,8 @@ function App() {
           openHospitalModal={openHospitalModal}
           openParkingModal={openParkingModal}
           closeModal={closeModal}
+          Modal_hospitalFavorite={Modal_hospitalFavorite}
+          Modal_parkingFavorite={Modal_parkingFavorite}
         />
       </div>
     </div>
