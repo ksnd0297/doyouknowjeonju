b5e428dd75c8b98dfc71ba2611b9f2aeb5340e4f
sangyong <tkddyd1126@gmail.com>
2022-11-29 01:48:42 +0900

    주석 수정

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -12,8 +12,6 @@ import "../../scss/Menu/Menu.scss";
 export const Menu = ({ menuSelect }) => {
   return (
     <div className="Menu_Main">
-      {/* onclick 이벤트를 통해 클릭할 때마다 담기는 값이 변경되도록 함. 변경되는 것은 pages state임
-      변경된 값은 Search에 적용되어 나타남 */}
       <div className="Menu_GoTag" onClick={() => menuSelect("tag")}>
         <MdLabel />
       </div>
